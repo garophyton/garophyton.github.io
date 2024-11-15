f85dc048f21f45777985b4da926fdc6834f22190
garophyton <gabi1537691199@gmail.com>
2024-11-15 11:47:27 -0300

    cambie un nombre erroneo

--- src/Home.js
+++ src/Home.js
@@ -1,8 +1,6 @@
 import React from 'react';
 import './styles.css';
 import Menu from './componentes/Menu';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faInstagram } from '@fortawesome/free-brands-svg-icons';
 
 const Home = () => {
   return (
@@ -10,7 +8,7 @@ const Home = () => {
       <Menu/>
       <div className="contenido-home">
         <img src="/imagenes/imgNews.jpeg" className='News' alt="news"/>
-        <img src="/imagenes/imgprincipal.jpeg" className='imgPrincipal' alt="imagenprincipal"/>
+        <img src="/imagenes/imgPrincipal.jpeg" className='imgPrincipal' alt="imagenprincipal"/>
         <img src="/imagenes/imgMasVendidas.jpeg" className='MV' alt="mas vendidas"/>
       </div>
     </>
